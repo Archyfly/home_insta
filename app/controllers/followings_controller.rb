66c1ebdaf4a4ed82d6be923b93d55c8a8c6cd563
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-08 01:26:16 +0800

    create model Follow, add controllers, add views, routes, self testing

---
+++ app/controllers/followings_controller.rb
@@ -0,0 +1,8 @@
+class FollowingsController < ActionController::Base
+
+  def index
+    @user = User.find(params[:user_id])
+    @followings = @user.followings
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_091136) do
+ActiveRecord::Schema.define(version: 2021_08_07_154948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2021_08_07_091136) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,5 +55,7 @@ ActiveRecord::Schema.define(version: 2021_08_07_091136) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "follows", "users", column: "follower_id"
+  add_foreign_key "follows", "users", column: "following_id"
   add_foreign_key "posts", "users"
 end
