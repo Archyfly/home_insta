8da17acaf5c12a42ae36358e49bd3b5f9738bf19
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-07 16:18:23 +0800

    add views for posts, self testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,23 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
-  before_action :find_post, only: %i[new create edit update destroy]
 
   def index
-    @user_posts = current_user.posts
     @posts = Post.all
   end
 
   def show
-    #@post = current_user.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     #user = User.find_by_id(@post.user_id)
     #@user_posts = user.posts.all
     #@post_comments = @post.comments
   end
 
   def new
-    #@post = current_user.posts.new
+    @post = current_user.posts.new
   end
 
   def create
-    #@post = current_user.posts.new(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to user_post_path(current_user, @post), flash: { success: 'Post was added! '}
     else
@@ -28,28 +26,28 @@ class PostsController < ApplicationController
   end
 
   def edit
-    #@post = current_user.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    #@post = current_user.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
-      render flash: { success: 'Post was updated! '}
+      redirect_to user_post_path(@post), flash: { success: 'Post was updated! '}
     else
-      render flash: { alert: 'Post NOT updated! '}
+      render :edit, flash: { alert: 'Post NOT updated! '}
     end
   end
 
   def destroy
-    #@post = current_user.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
     redirect_to root_path, flash: { success: 'Post was deleted! '}
   end
 
   private
 
-  def find_post
-    @post = current_user.posts.find(params[:id])
+  def find_user
+    @user = current_user
   end
 
   def post_params
@@ -58,5 +56,3 @@ class PostsController < ApplicationController
 
 end
 
-
-end
