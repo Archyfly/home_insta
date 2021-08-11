cffd0735289a7bc63ca9f44d1503adb5d222ba4f
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-11 17:02:18 +0800

    change root path to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#all_posts'
+  root 'users#index'
 
   resources :users do#, only: [:index, :show, :edit, :update]
     resources :posts do
