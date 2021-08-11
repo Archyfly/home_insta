a8299ba44dd9592f6aded1187755d495f311eadc
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-11 21:39:14 +0800

    add simple tests of presence for users, posts. add factory post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   #include ImageUploader::Attachment[:image]
 
-  belongs_to :user
+  belongs_to :user, required: true
   has_many :comments
   has_many :photos
 
   validates :title, :body, presence: true
-  validates :title, length: { minimum: 5}
+  validates :title, length: { minimum: 5 }
   validates :title, length: { maximum: 30, too_long: "30 characters is the maximum allowed" }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,12 @@
+require 'ffaker'
+
 FactoryBot.define do
   factory :user do
-    username { "Viktor Petlura" }
-    info { "White guardsman"}
+    username { FFaker::Name.name }
+    email { FFaker::Internet.email }
+    info { FFaker::Job.title }
+    password { 'password' }
+    password_confirmation { 'password' }
 
   end
 end
