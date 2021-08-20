37d2660b80e41b05349576f0d9018560392717d0
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-21 02:22:33 +0800

    add index FollowerId FollowingId

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,4 +2,5 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User", foreign_key: :follower_id
   belongs_to :following, class_name: "User", foreign_key: :following_id
 
+  #validates :follow, uniqueness:  { :scope => [:follower_id, :following_id] }
 end
---
+++ db/migrate/20210820182111_add_index_follower_id_followings_id.rb
@@ -0,0 +1,4 @@
+class AddIndexFollowerIdFollowingsId < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
